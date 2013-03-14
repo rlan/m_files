36ca4f245401ca711e9825f4c4ff4a7470883278
Rick Lan <rlan@users.noreply.github.com>
2013-03-13 21:55:15 -0700

    Support zero size guards.

--- kfft.m
+++ kfft.m
@@ -1,15 +1,18 @@
-function [k]=kfft(n_fft, n_dc_guard, n_nyquist_guard, is_shifted)
+function [k]=kfft(n_fft, n_dc_guard, n_nyquist_guard, shifted)
 %KFFT Return FFT bin indices.
 %   k = KFFT(n_fft, n_dc_guard, n_nyquist_guard, shifted)
 %      n_fft: FFT size
-%      n_dc_guard: Number of DC guard bins
-%      n_nyquist_guard: Number of nyquist guard bins
+%      n_dc_guard: Number of DC guard bins.
+%      n_nyquist_guard: Number of nyquist guard bins.
 %      shifted: Omit for all positive bin values. Otherwise
 %               bins >= Nyquist are negative.
 %   In general, this is used to get the bin indices for OFDM
 %   systems.
 %
 %   Examples:
+%
+%   >> kfft(256, 0, 0)
+%   returns [0:255]'
 %
 %   >> kfft(256, 4, 12)
 %   returns [4:115, 141:252]'
@@ -22,16 +25,20 @@ function [k]=kfft(n_fft, n_dc_guard, n_nyquist_guard, is_shifted)
 %
 error(nargchk(3, 4, nargin));
 
-start = n_dc_guard;
-stop = n_fft/2 - 1 - n_nyquist_guard;
-lower_half = [start:stop]';
+lower = [0:n_fft/2-1]';
+upper = [n_fft/2:n_fft-1]';
+%[min(lower), max(lower), min(upper), max(upper)]
 
-start = n_fft/2 + n_nyquist_guard + 1;
-stop = n_fft-1 - (n_dc_guard - 1);
-if nargin >= 4
-    start = start - n_fft;
-    stop = stop - n_fft;
+if (n_dc_guard > 0) | (n_nyquist_guard > 0)
+    len = length(lower);
+    lower = lower(n_dc_guard+1:len-n_nyquist_guard);
+    upper = upper((n_nyquist_guard+1)+1:len-(n_dc_guard-1));
+    %[min(lower), max(lower), min(upper), max(upper)]
 end
-upper_half = [start:stop]';
 
-k = [lower_half; upper_half];
+if nargin >= 4
+    upper = upper - n_fft;
+    %[min(lower), max(lower), min(upper), max(upper)]
+end
+
+k = [lower;upper];
